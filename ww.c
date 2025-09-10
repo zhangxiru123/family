3a274243529d0e81209bc5a69b547ba42672333e
zxr <2944613897@qq.com>
2025-09-10 20:12:15 +0800

    v1.1

--- ww.c
+++ ww.c
@@ -4,7 +4,7 @@
 int main()
 {
     int zhang,xi;              //变量声明
-    zhang=3856;                   //赋值
+    zhang=3856;                   //赋值，哈哈哈
     xi=zhang*zhang*zhang;       //计算体积，哈哈哈
     printf("边长:%d\n",zhang);
     printf("体积:%d\n",xi);
