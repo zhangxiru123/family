5ed729225108f5df9e2a06a0adbde7249ba76b59
zxr <2944613897@qq.com>
2025-09-10 19:34:59 +0800

    www

---
+++ ww.c
@@ -0,0 +1,13 @@
+/*volume.c*/
+/*给出正方形的边长，求体积*/
+#include<stdio.h>
+int main()
+{
+    int zhang,xi;              //变量声明
+    zhang=3856;                   //赋值
+    xi=zhang*zhang*zhang;       //计算体积，哈哈哈
+    printf("边长:%d\n",zhang);
+    printf("体积:%d\n",xi);
+    return 0;                    
+    
+}
